8dab4a4ff791409a533dbd47d96077785e1ac3b2
DJKai <djkai.mcpe@gmail.com>
2016-01-17 00:20:28 +0000

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,1 +1,20 @@
-
+<?php
+namespace JoinMessageOTD;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\Server;
+ 
+class Main extends PluginBase {
+	public function onRun($player)
+	{
+		        $player = $this->player;
+                $player->sendMessage("Welcome to my Server!");
+	}
+        public function onEnable (){
+        $this->getLogger()->info("I've been loaded!");
+               
+        }
+       
+}
